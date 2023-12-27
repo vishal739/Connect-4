d90257ee578e6aff30778cab6732a1955ab1e13e
vishal739 <vishalsinghkr739@gmail.com>
2023-12-27 15:34:48 +0530

    Uploading Connect4

--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -17,13 +17,13 @@ import {
 const GameBoard = () => {
     const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
     const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1);
-    const [gameState,setGameState]=useState(GAME_PLAYING);
-    const [winPlayer,setWinPlayer]= useState(NO_PLAYER);
-    
-    useEffect(()=>{
+    const [gameState, setGameState] = useState(GAME_PLAYING);
+    const [winPlayer, setWinPlayer] = useState(NO_PLAYER);
+
+    useEffect(() => {
         initGame();
-    },[]);
-    const initGame= () =>{
+    }, []);
+    const initGame = () => {
         console.log("init");
         //debugger;;
         setGameBoard(Array(16).fill(NO_PLAYER));
@@ -32,28 +32,28 @@ const GameBoard = () => {
         setWinPlayer(NO_PLAYER);
     }
 
-    const initCircle = () =>{
-        const circle =[];
-        for(var i=0; i<NO_CIRCLE; i++){
+    const initCircle = () => {
+        const circle = [];
+        for (var i = 0; i < NO_CIRCLE; i++) {
             circle.push(renderCircle(i));
         }
         return circle;
     };
 
-    const computerPlay = () =>{
+    const computerPlay = () => {
         console.log("clicked");
-        CircleHandle(getComputerMove(gameBoard,currentPlayer));
+        CircleHandle(getComputerMove(gameBoard, currentPlayer));
     }
 
     const CircleHandle = (id) => {
-        if(gameBoard[id]!==0) return;
-        if(gameState!==GAME_PLAYING) return;
-        if(isWinner(gameBoard,id,currentPlayer)){
+        if (gameBoard[id] !== 0) return;
+        if (gameState !== GAME_PLAYING) return;
+        if (isWinner(gameBoard, id, currentPlayer)) {
             console.log("Winner");
             setGameState(GAME_WON);
             setWinPlayer(currentPlayer);
         }
-        if(isDraw(gameBoard,id,currentPlayer)){
+        if (isDraw(gameBoard, id, currentPlayer)) {
             console.log("Game Draw");
             setGameState(GAME_DRAW);
             setWinPlayer(NO_PLAYER);
@@ -74,13 +74,13 @@ const GameBoard = () => {
             <GameCircle key={id} className={`Player_${gameBoard[id]}`} id={id} onCircleHandle={CircleHandle} />)
     }
     return (
-        <>
-        <GameHeader gameState={gameState} currentPlayer={currentPlayer} winPlayer={winPlayer}/>
-        <div className="GameBoard">
-            {initCircle()}
+        <div className='container'>
+            <GameHeader gameState={gameState} currentPlayer={currentPlayer} winPlayer={winPlayer} />
+            <div className="GameBoard">
+                {initCircle()}
+            </div>
+            <GameFooter onNewGameClick={initGame} onComputerClick={computerPlay} gameState={gameState} />
         </div>
-        <GameFooter onNewGameClick={initGame} onComputerClick={computerPlay} gameState={gameState}/>
-        </>
     )
 }
 
